27053417167a1f03e4706683fdf8065c1d52e0cb
zhenpanda <zhentzhou@gmail.com>
2014-08-17 21:12:04 -0400

    first commit
    setup general styles and variables

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+	require_once('inclues/common.inc');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" href="styles/style.css">
+
+	<title>Home | <?=SITE_NAME?></title>
+</head>
+<body>
+	
+</body>
+</html>
