cf98ce076a1e64c76d2fcabebc80535e6392d94b
zhenpanda <zhentzhou@gmail.com>
2014-10-21 15:54:39 -0400

    Basic footer build

--- includes/footer.php
+++ includes/footer.php
@@ -1,29 +1,76 @@
 <footer class="footer-wrap">
 		
 	<div class="container">
+		<div class="box"></div>
 		<div class="row">
 			<div class="col-xs-12">
+			<!-- |________________| -->
 				<?=TITLE_MARKUP;?>
 			</div>
 		</div>
-
+		<div class="box"></div>
 		<div class="row">
 			<div class="col-xs-4">
-				
+			<!-- |__| |__| |__| |__| -->
 				<div class="row">
 					<div class="col-xs-6">
+					<!-- |_||_| |__| |__| |__| -->
 						<h6 class="ryb-black">法拉盛分校</h6>
 						<p>
 							136-18 39Ave, 9FL<br />
 							Flushing NY 11354
 						</p>
 					</div>
-					<div class="col-xs-6"></div>
-				
+					<div class="col-xs-6">
+						<h6 class="ryb-black">艾姆赫斯特</h6>
+						<p>
+							83-25 Broadway, #3F<br />
+							Elmhurst NY 11373
+						</p>
+					</div>
+					<div class="col-xs-6">
+						<h6 class="ryb-black">唐人街分校</h6>
+						<p>
+							265 Canal St, #401<br />
+							NewYork, NY 10013
+						</p>
+					</div>						
+					<div class="col-xs-6">
+						<h6 class="ryb-black">布碌崙分校</h6>
+						<p>
+							6402 8th Ave, #220<br />
+							Brooklyn NY 11220
+						</p>
+					</div>	
+					<div class="col-xs-6">
+						<h6 class="ryb-black">來電垂詢</h6>
+						<p>
+							1-718-886-4008<br />
+							1-917-822-4139
+						</p>
+					</div>	
 				</div>
 			</div>
-			<div class="col-xs-4"></div>
-			<div class="col-xs-4"></div>
+			<div class="col-xs-4">
+				<!-- |__| |_||_| |__| |__| -->
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">學校簡介</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">教師招聘</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">教育目標</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">學校新聞</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">我們的老師</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">學生登錄</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">課程註冊</h5> </div>
+					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">聯絡網頁</h5> </div>
+
+					<div class="col-xs-12 smallbox"> <h5 class="ryb-brown">參觀學校</h5> </div>
+					<div class="col-xs-12 smallbox"> <h5 class="ryb-brown">藝術畫廊</h5> </div>
+			</div>		
+			<div class="col-xs-4">
+				<!-- |__| |__| |__| |__| -->
+			     <a class="logo" href="#">
+			     	<img src="images/logoFull2.png" alt="logo" />
+			     </a>
+			</div>
 		</div>
 	</div>
 
--- includes/header.php
+++ includes/header.php
@@ -10,15 +10,15 @@
         <span class="icon-bar"></span>
       </button>
       <a class="logo" href="#">
-      	<img src="images/logoFull.png" alt="logo" />
+      	<img src="images/logoFull2.png" alt="logo" />
       </a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="ryb-navbar">
       <ul class="nav navbar-nav">
-        <li><a href="#">藝術畫廊</a></li>
         <li><a href="#">學校簡介</a></li>
+        <li><a href="#">報名信息</a></li>
         <li><a href="#">學生登錄</a></li>
         <li><a href="#">藝術畫廊</a></li>
         <li><a href="#">聯絡網頁</a></li>
