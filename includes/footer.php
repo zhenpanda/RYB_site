0fb8b8b45972052e6f17327c68c82efbd29cdb8d
zhenpanda <zhentzhou@gmail.com>
2014-11-02 21:46:40 -0500

    finished footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,72 +1,91 @@
 <footer class="footer-wrap">
 		
 	<div class="container">
-		<div class="box"></div>
 		<div class="row">
 			<div class="col-xs-12">
 			<!-- |________________| -->
 				<?=TITLE_MARKUP;?>
 			</div>
 		</div>
-		<div class="box"></div>
-		<div class="row">
+
+		<div class="row row-big">
 			<div class="col-xs-4">
-			<!-- |__| |__| |__| |__| -->
+			<!-- |___| |___| |___| -->
 				<div class="row">
 					<div class="col-xs-6">
-					<!-- |_||_| |__| |__| |__| -->
-						<h6 class="ryb-black">法拉盛分校</h6>
-						<p>
-							136-18 39Ave, 9FL<br />
-							Flushing NY 11354
-						</p>
+					<!-- |_||_| |__| |__| -->
+
+						<a href="#" class="location">
+							<h6 class="ryb-black">法拉盛分校</h6>
+							<p>
+								136-18 39Ave, 9FL<br />
+								Flushing NY 11354
+							</p>
+						</a>
+
+						<a href="#" class="location">
+							<h6 class="ryb-black">艾姆赫斯特</h6>
+							<p>
+								83-25 Broadway, #3F<br />
+								Elmhurst NY 11373
+							</p>			
+						</a>
+
+						<a href="#" class="location">	
+							<h6 class="ryb-black">來電垂詢</h6>
+							<p>
+								1-718-886-4008<br />
+								1-917-822-4139
+							</p>	
+						</a>
 					</div>
+				
 					<div class="col-xs-6">
-						<h6 class="ryb-black">艾姆赫斯特</h6>
-						<p>
-							83-25 Broadway, #3F<br />
-							Elmhurst NY 11373
-						</p>
-					</div>
-					<div class="col-xs-6">
-						<h6 class="ryb-black">唐人街分校</h6>
-						<p>
-							265 Canal St, #401<br />
-							NewYork, NY 10013
-						</p>
-					</div>						
-					<div class="col-xs-6">
-						<h6 class="ryb-black">布碌崙分校</h6>
-						<p>
-							6402 8th Ave, #220<br />
-							Brooklyn NY 11220
-						</p>
-					</div>	
-					<div class="col-xs-6">
-						<h6 class="ryb-black">來電垂詢</h6>
-						<p>
-							1-718-886-4008<br />
-							1-917-822-4139
-						</p>
+
+						<a href="#" class="location">
+							<h6 class="ryb-black">布碌崙分校</h6>
+							<p>
+								6402 8th Ave, #220<br />
+								Brooklyn NY 11220
+							</p>
+						</a>
+
+						<a href="#" class="location">
+							<h6 class="ryb-black">唐人街分校</h6>
+							<p>
+								265 Canal St, #401<br />
+								New York, NY 10013
+							</p>		
+						</a>
+
 					</div>	
 				</div>
 			</div>
-			<div class="col-xs-4">
-				<!-- |__| |_||_| |__| |__| -->
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">學校簡介</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">教師招聘</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">教育目標</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">學校新聞</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">我們的老師</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">學生登錄</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">課程註冊</h5> </div>
-					<div class="col-xs-6 smallbox"> <h5 class="ryb-brown">聯絡網頁</h5> </div>
 
-					<div class="col-xs-12 smallbox"> <h5 class="ryb-brown">參觀學校</h5> </div>
-					<div class="col-xs-12 smallbox"> <h5 class="ryb-brown">藝術畫廊</h5> </div>
-			</div>		
+			<div class="col-xs-4">	
+				<div class="row">
+					<div class="col-xs-6">
+						<ul>
+							<li><a href="#" class="h5">學校簡介</a></li>
+							<li><a href="#" class="h5">教師招聘</a></li>
+							<li><a href="#" class="h5">教育目標</a></li>
+							<li><a href="#" class="h5">學校新聞</a></li>
+							<li><a href="#" class="h5">我們的老師</a></li>
+							<li><a href="#" class="h5">學生登錄</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-6">
+						<ul>
+							<li><a href="#" class="h5">課程註冊</a></li>
+							<li><a href="#" class="h5">聯絡網頁</a></li>
+							<li><a href="#" class="h5">參觀學校</a></li>
+							<li><a href="#" class="h5">藝術畫廊</a></li>
+						</ul>
+					</div>					
+				</div>
+			</div>
 			<div class="col-xs-4">
-				<!-- |__| |__| |__| |__| -->
+				<!-- |__| |__| |__| -->
 			     <a class="logo" href="#">
 			     	<img src="images/logoFull2.png" alt="logo" />
 			     </a>
