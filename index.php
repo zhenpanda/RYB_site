c2894efd18b905253ccb42364fae98253bc3d03e
zhenpanda <zhentzhou@gmail.com>
2014-11-13 18:01:24 -0500

    more setup on front page

--- index.php
+++ index.php
@@ -8,37 +8,91 @@
 <body>
 
 	<?php require_once('includes/header.php');?>
+	<!-- Nav Bar -->
 
 	<div class="container main-container"> 
-
+		
+		<!--|____________| -->
 		<div class="row">
-		<!--| Main Page Image | -->
-			<div class="col-xs-12"> 
-				<img src="images/mainPagePic.png" alt=""> 
+
+			<!-- Main Page Image -->
+			<div class="col-xs-12 spacing-reset page-center"> <img src="images/mainPagePic.png" alt=""> </div>
+
+			<!-- |___||___||___||___| -->
+			<div class="col-xs-12 spacing-reset blockNudge">
+				
+				<!-- Block links -->			 
+				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/missionBlock.png" alt=""> </a> </div>
+				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/applyBlock.png" alt=""> </a> </div>
+				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/visitBlock.png" alt=""> </a> </div>
+				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/teacherBlock.png" alt=""> </a> </div>
 			</div>
 
-			<div class="col-xs-12 blockNudge">
-				<!-- |   ||   ||   ||   | --> 
-				<div class="col-xs-3 spacing-reset blockSpacing">
-					<a href="#" class=""> <img src="images/missionBlock.png" alt=""> </a>
-				</div>
-				<div class="col-xs-3 spacing-reset blockSpacing"> 
-					<a href="#" class=""> <img src="images/applyBlock.png" alt=""> </a>
-				</div>
-				<div class="col-xs-3 spacing-reset blockSpacing"> 
-					<a href="#" class=""> <img src="images/visitBlock.png" alt=""> </a>
-				</div>
-				<div class="col-xs-3 spacing-reset blockSpacing"> 
-					<a href="#" class=""> <img src="images/teacherBlock.png" alt=""> </a>
+			<!-- |____________| -->			
+			<div class="col-xs-12 spacing-reset mid-frontpage">
+			<!--  Mid Page Area  -->
+
+				<!-- Mid Page Title -->
+				<div class="col-xs-12 spacing-reset"> <h1 class="">學校新聞</h6> </div>
+				<!-- Mid Page Title Divider -->
+				<div class="col-xs-12 mid-divider-light"> </div>
+
+				<!-- |____||____||____| -->
+				<div class="col-xs-12 spacing-reset">
+				<!-- Teaser Top -->	
+					
+					<!-- Teaser text 1, Teaser test 2 -->
+					<div class="col-xs-4 spacing-reset"> 
+						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
+						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
+					</div>	
+					<!-- Mid Page small Image 1, Mid Page small Image 2 -->
+					<div class="col-xs-12 spacing-reset mid-page-image"> 
+						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall1.png" alt=""> </a>
+						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall2.png" alt=""> </a>
+					</div>
+				</div>	
+				
+				<!-- |____||____||____| -->
+				<div class="col-xs-12 spacing-reset">
+				<!-- Teaser Bottom -->	
+					
+					<!-- Teaser text 3, Teaser test 4,  Teaser test 5-->
+					<div class="col-xs-4 spacing-reset"> 
+						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
+						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
+						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
+					</div>					
+					<!-- Mid Page small Image 3, Mid Page small Image 4 -->
+					<div class="col-xs-12 spacing-reset mid-page-image"> 
+						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall3.png" alt=""> </a>
+						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall4.png" alt=""> </a>
+					</div>
+				</div>			
+			</div>
+
+			<!-- |____________| -->	
+			<div class="col-xs-12 spacing-reset bottom-frontpage">
+			<!-- Bottom Page Area-->
+				
+				<!-- Bottom Page Title -->
+				<div class="col-xs-12 spacing-reset"> <h1 class="">學習    -    啟發     -    創作</h6> </div>
+				<div class="col-xs-12 spacing-reset mid-divider-dark"> </div>		
+				
+				<!-- |____||____||____| -->
+				<div class="col-xs-4 spacing-reset bottom-image-set"> 
+				<!-- Bottom Page Image -->
+					<a href="#" class="spacing-reset"> <img src="images/mainPagePicCircle1.png" alt=""> </a>
+					<a href="#" class="spacing-reset bottom-mid-image"> <img src="images/mainPagePicCircle2.png" alt=""> </a>
+					<a href="#" class="spacing-reset"> <img src="images/mainPagePicCircle3.png" alt=""> </a>
 				</div>
 			</div>
 
-		</div>
-			
 		</div>
 	</div>
 	
 	<?php require_once('includes/footer.php');?>
+	<!-- Footer -->
 
 </body>
 </html>
