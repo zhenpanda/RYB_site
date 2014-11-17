326f16bec16761daf773665b2bd5c732ff77a648
zhenpanda <zhentzhou@gmail.com>
2014-11-16 22:29:46 -0500

    clean up homepage

--- index.php
+++ index.php
@@ -10,84 +10,90 @@
 	<?php require_once('includes/header.php');?>
 	<!-- Nav Bar -->
 
-	<div class="container main-container"> 
+	<div class="container homepage-top"> 
 		
 		<!--|____________| -->
 		<div class="row">
 
 			<!-- Main Page Image -->
-			<div class="col-xs-12 spacing-reset page-center"> <img src="images/mainPagePic.png" alt=""> </div>
+			<div class="col-xs-12 text-center">
+				<img src="images/mainPagePic.png" alt="" /> 
+			</div>
 
-			<!-- |___||___||___||___| -->
-			<div class="col-xs-12 spacing-reset blockNudge">
-				
+			<div class="col-xs-12 text-center homepage-links">
 				<!-- Block links -->			 
-				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/missionBlock.png" alt=""> </a> </div>
-				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/applyBlock.png" alt=""> </a> </div>
-				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/visitBlock.png" alt=""> </a> </div>
-				<div class="col-xs-3 spacing-reset blockSpacing"> <a href="#" class=""> <img src="images/teacherBlock.png" alt=""> </a> </div>
+				<a href="#" class=""><img src="images/missionBlock.png" alt=""></a><a href="#" class=""><img src="images/applyBlock.png" alt=""></a><a href="#" class=""><img src="images/visitBlock.png" alt=""></a><a href="#" class=""><img src="images/teacherBlock.png" alt=""></a>
 			</div>
+		</div>
+	</div>
 
-			<!-- |____________| -->			
-			<div class="col-xs-12 spacing-reset mid-frontpage">
-			<!--  Mid Page Area  -->
-
-				<!-- Mid Page Title -->
-				<div class="col-xs-12 spacing-reset"> <h1 class="">學校新聞</h6> </div>
-				<!-- Mid Page Title Divider -->
-				<div class="col-xs-12 mid-divider-light"> </div>
-
-				<!-- |____||____||____| -->
-				<div class="col-xs-12 spacing-reset">
-				<!-- Teaser Top -->	
-					
-					<!-- Teaser text 1, Teaser test 2 -->
-					<div class="col-xs-4 spacing-reset"> 
-						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
-						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
-					</div>	
-					<!-- Mid Page small Image 1, Mid Page small Image 2 -->
-					<div class="col-xs-12 spacing-reset mid-page-image"> 
-						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall1.png" alt=""> </a>
-						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall2.png" alt=""> </a>
-					</div>
-				</div>	
-				
-				<!-- |____||____||____| -->
-				<div class="col-xs-12 spacing-reset">
-				<!-- Teaser Bottom -->	
-					
-					<!-- Teaser text 3, Teaser test 4,  Teaser test 5-->
-					<div class="col-xs-4 spacing-reset"> 
-						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
-						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
-						<a href="#" class="spacing-reset"> <div class="col-xs-12 spacing-reset teaser-block"> </div> </a>
-					</div>					
-					<!-- Mid Page small Image 3, Mid Page small Image 4 -->
-					<div class="col-xs-12 spacing-reset mid-page-image"> 
-						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall3.png" alt=""> </a>
-						<a href="#" class="spacing-reset"> <img src="images/mainPagePicSmall4.png" alt=""> </a>
-					</div>
-				</div>			
-			</div>
-
-			<!-- |____________| -->	
-			<div class="col-xs-12 spacing-reset bottom-frontpage">
-			<!-- Bottom Page Area-->
-				
-				<!-- Bottom Page Title -->
-				<div class="col-xs-12 spacing-reset"> <h1 class="">學習    -    啟發     -    創作</h6> </div>
-				<div class="col-xs-12 spacing-reset mid-divider-dark"> </div>		
-				
-				<!-- |____||____||____| -->
-				<div class="col-xs-4 spacing-reset bottom-image-set"> 
-				<!-- Bottom Page Image -->
-					<a href="#" class="spacing-reset"> <img src="images/mainPagePicCircle1.png" alt=""> </a>
-					<a href="#" class="spacing-reset bottom-mid-image"> <img src="images/mainPagePicCircle2.png" alt=""> </a>
-					<a href="#" class="spacing-reset"> <img src="images/mainPagePicCircle3.png" alt=""> </a>
+	<div class="homepage-middle-wrapper">
+		<!--middle page wrapper for container -->
+		<div class="container homepage-middle">
+			
+			<div class="row">
+			<!-- middle row -->			
+				<div class="col-xs-12">
+					<div class="h1 underline-title">學校新聞</div>
 				</div>
-			</div>
 
+				<div class="col-xs-4">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="teaser-block"><a href="#"></a></div>
+							<div class="teaser-block"><a href="#"></a></div>
+							<div class="teaser-block"><a href="#"></a></div>
+							<div class="teaser-block"><a href="#"></a></div>
+							<div class="teaser-block"><a href="#"></a></div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-8">
+					<div class="row homepage-image-row">
+						<div class="col-xs-12">
+							<a href="#"> <img src="images/mainPagePicSmall1.png" alt=""> </a><a href="#"> <img src="images/mainPagePicSmall2.png" alt=""> </a>
+						</div>
+					</div>
+					<div class="row homepage-image-row">
+						<div class="col-xs-12">
+							<a href="#"> <img src="images/mainPagePicSmall3.png" alt=""> </a><a href="#"> <img src="images/mainPagePicSmall4.png" alt=""> </a>
+						</div>
+					</div>
+				</div>
+
+			</div>		
+		</div>
+	</div>
+
+	<div class="container homepage-bottom">
+		<div class="row">
+		<!-- Bottom Page Area-->
+			<div class="col-xs-12">		
+				<!-- Bottom Page Title -->
+				<div class="h1 underline-title underline-title-bottom">學習    -    啟發     -    創作</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-4 text-center">
+				<a href="#" class="circle-image-link">
+					<img src="images/mainPagePicCircle1.png" alt="">
+					<div class="h1">學生故事</div>
+				</a>
+			</div>
+			<div class="col-xs-4 text-center">
+				<a href="#" class="circle-image-link">
+					<img src="images/mainPagePicCircle2.png" alt="">
+					<div class="h1">學校歷史</div>
+				</a>
+			</div>
+			<div class="col-xs-4 text-center">
+				<a href="#" class="circle-image-link">
+					<img src="images/mainPagePicCircle3.png" alt="">
+					<div class="h1">藝術創意</div>
+				</a>
+			</div>
 		</div>
 	</div>
 	
