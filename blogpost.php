22bdfcf264ca19bd9ca2bbbd2842269426190630
zhenpanda <zhentzhou@gmail.com>
2014-11-25 18:13:52 -0500

    worked on blogpost page

---
+++ blogpost.php
@@ -0,0 +1,63 @@
+<?php
+	require_once('includes/common.inc');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once('includes/head.php');?>
+
+<body>
+
+	<?php require_once('includes/header.php');?>
+
+	<!--Blog Banner -->	
+	<div class="blog-banner">
+		<div class="container"> 
+			<div class="row">
+				<!--|____________|-->
+				<div class="col-xs-12">
+					<div class="h1 color-title blog-underline"><span class="ryb-red">紅</span><span class="ryb-blue">黃</span><span class="ryb-yellow">藍</span><span class="ryb-purple">美術學校</span><span class="ryb-brown">秋季班招生</span></div>
+				</div>
+				<!--|_________||___|-->
+				<div class="col-xs-">
+					<div class="h5 text-right">更新於2014年9月23日，上午10時50分美東時間</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--Main Content -->
+	<div class="container "> 
+		<div class="row">
+			<!--|_________||___|-->
+			<div class="col-xs-9 "> 
+				<div class="text-center"><img src="images/KidsClassroom.png" alt="" /></div>
+
+				<div class="blog-text-block">
+					<div class="row">
+
+						<div class="col-xs-12 h7 blog-text-space-big">藝術名校紅黃藍美術學校自創辦十五年來以重視繪畫基本功训练享譽美東華人社區。該校自創辦以來就有一整套完整的基本功訓練和想像力創意相結合的繪畫教学系统 。專業和極具實力的教師隊伍是紅黃藍美術學校教学质量的保证，所有的老師按不同的教學要求特聘來校任教。老师精通英文，国语，和广东话。学校教学环境优雅，整洁极具艺术氛围。目前秋季来临，我校在法拉盛，唐人街，布鲁仑和艾姆赫斯特的四所分校开始招收秋季班的学生。希望前来免费试课和报名。</div>
+						<div class="col-xs-12 h7 blog-text-space-small">紅黃藍美術學校2014年秋季班教学课程内容现推出。教學類容分六個部分。第一部分是兒童畫；這是針對4至7歲的小朋友，主要學習兒童畫，兒童水彩，儿童创意画。 這一部分主要是學会掌握正確的繪畫姿勢和正確的繪畫步驟， 學會掌握基本的</div>	
+						
+						<div class="col-xs-4 blog-stand-in-block"></div>
+						
+						<div class="col-xs-8 h7 blog-text-space-big">構圖和比例及最简单的造型能力，学会使用各种美术材料和美术用具，学会画面中个体的表现和多种物体的表现，学会与美术老师及同学间的艺术交流。第二部分是素描画；主要是针对8至12岁的小朋友，素描是一切造型艺术的基础，学好素描是了为打好绘画基本功的必修课程。素描学习从临摹开始解决最基本的绘画步骤，起笔落笔，构图要求，明暗关系。掌握了素描临摹后再开始学习素描写生，素描写生抱括静物写生，风景写生，人物写生三大部分。通过写生解决素描的造型能力，表现技巧，如素描定位，</div>
+						<div class="col-xs-12 h7 blog-text-space-big">素描定型，素描五调子，素描立体感，素描的线条, 素描的明暗和正确的写生观察方法。素描写生课程完成后再开始素描创作课程学习，通过学习素描创作来提高想象力和绘画艺术的表现能力，为将来走专业的绘画道路打好扎实的基本功。第三部分是色彩画；这部分针对8岁至成人的学生，它包括油画，水彩画，亚克力画 ，卡通画，粉彩画，国画等 ，这部分要学习色彩的冷暖关系，色彩造型，色彩表现，各类画种的绘画笔触和技巧，学会使用各类画种的不同材料和工具。由于画种繁多这部分有待针对性的具体讲解。 </div>
+
+						<div class="col-xs-8 h7 blog-text-space-big">第四部分是各类艺术大学艺术高中和艺术初中的考试辅导课程 很多学画的学生希望报考各类的艺术高中初中和艺术类大学，在选择学校和专业方面 ，在前往报考时所需要的作品准备方面我校会给予很完整系统的辅导帮助 ，十几年来我们有95% 的成功率 。第五部分是各类画展，各类绘画比赛，绘画考级，绘画创作的辅导。绘画比赛和参加各类画展是提高绘画积极性最重要的方法。也是评定学画学生绘画水平的手段之一。每年在我校老师的辅导下很多学生取得了各类绘画大赛的很好成绩。</div>
+						<div class="col-xs-4 h7 blog-stand-in-block"></div>
+
+					</div>	
+				</div>
+
+			</div>
+			
+			<!--|_________||___|-->
+			<div class="col-xs-3"></div>
+		</div>
+	</div>
+
+	
+	<?php require_once('includes/footer.php');?>
+
+</body>
+</html>
