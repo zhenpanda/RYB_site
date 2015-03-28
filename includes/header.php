f551e0023a1bcbef711e1ada3ae2f6f9e1e3f95f
zhenpanda <zhentzhou@gmail.com>
2015-03-28 19:08:19 -0400

    mobile button added

--- includes/header.php
+++ includes/header.php
@@ -7,25 +7,16 @@ require_once('content/header.inc');
   <nav class="navbar navbar-default" role="navigation">
     <div class="container">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ryb-navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a class="logo" href="#"><img src="images/Home Page Img Folder/logo.png" alt="logo" /></a>
       </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="ryb-navbar">
-        <ul class="nav navbar-nav">
-          <?php foreach($header_links as $count => $link): ?>
-            <li><a href="/<?=$link ?>"><?=$header_titles[$language][$count]?></a></li>
-          <?php endforeach; ?>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="ryb-navbar">
+          <ul class="nav navbar-nav">
+            <?php foreach($header_links as $count => $link): ?>
+              <li><a href="/<?=$link ?>"><?=$header_titles[$language][$count]?></a></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
   </nav>
 </div>
 
@@ -34,10 +25,27 @@ require_once('content/header.inc');
   <nav class="navbar navbar-moblie" role="navigation">
     <div class="container">
       <div class="navbar-header">
-        <a class="mobile-logo" href="#">
+        <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#ryb-navbar-mobile">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a class="mobile-logo pull-right" href="#">
           <img src="images/mobile Img Folder/mobile-logo.png" alt="logo" />
         </a>
       </div>
     </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="ryb-navbar-mobile">
+      <ul class="nav navbar-nav">
+        <?php foreach($header_links as $count => $link): ?>
+          <li><a href="/<?=$link ?>"><?=$header_titles[$language][$count]?></a></li>
+        <?php endforeach; ?>
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
   </nav>
 </div>
