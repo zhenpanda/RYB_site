01c917c0293483bb85809d1109e51edfdda0a539
zhenpanda <zhentzhou@gmail.com>
2015-05-19 18:08:39 -0400

    added contact omg

--- contact.php
+++ contact.php
@@ -33,6 +33,28 @@
 	</div>
 	</div>
 
+	<div class="container postit-block hidden-xs">
+		<!--|____________| -->
+		<div class="row">
+			<div class="col-xs-3 text-center hidden-xs"></div>
+			<div class="col-xs-6 postit-yellow hidden-xs">
+				<img class="img-responsive img-center" src="/images/Contact_Img_folder/postItBig.png" alt="">
+				<h1 class="postit-text">法拉盛地址:</h1>
+				<h2 class="postit-text">36-26 Main St. 2X<br>Flushing, NY, 11354<br><br>联系电话:<br>718-886-4008<br>917-822-4139<br>917-417-8014</h2>
+			</div>
+			<div class="col-xs-3 text-center hidden-xs"></div>
+		</div>
+	</div>
+	</div>
+
+	<div class="container postit-block hidden-xs">
+		<!--|____________| -->
+		<div class="row">
+			<div class="col-xs-4 text-center hidden-xs"></div>
+		</div>
+	</div>
+	</div>
+
 	<?php require_once('includes/footer.php');?>
 
 </body>
