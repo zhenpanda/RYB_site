c6c2a7a02405885c065b1c62ce5f4e104f09f413
zhenpanda <zhentzhou@gmail.com>
2016-01-30 22:35:28 -0500

    test

--- index.php
+++ index.php
@@ -15,80 +15,82 @@
 	
 	<!-- **************************************************************************************************** -->	
 	<!-- Desktop main Content of homepage -->
-
-	<div class="container homepage-top"> 
-		<!--|____________| -->
-		<div class="row">
-
-			<!-- Desktop display image -->
-			<div class="col-xs-12 text-center">
-				<?php if($language == "en"): ?> 
-					<img class="img-responsive full-width" src="/images/homepage_img_folder/mainPagePicENG.jpg" alt="" />
-				<?php elseif($language == "zh-hans") : ?>
-				    <img class="img-responsive full-width" src="/images/homepage_img_folder/mainPagePicCHI.jpg" alt="" />
-				<?php endif; ?>
-			</div>
-			<div class="col-xs-12 text-center homepage-links">
-			<!-- Block links -->
-				<div class="row homepage-links">
-					<div class="col-xs-10 col-xs-offset-1">
-						<!-- Use PHP to loop through an array to create link block images -->
-						<?php for($count = 0; $count < count($box_images); $count++): ?>
-		          			<div class="col-xs-3 block-link">
-		          				<a href="<?=$homepage_block_links[$count]?>" class="">
-		          					<img class="img-responsive" src="/images/homepage_img_folder/<?=$box_images[$count] ?>" alt="">
-		          					<h3 class="box-text"><?=$boxes_links[$language][$count]?></h3>
-		          				</a>
-		          			</div>
-		        		<?php endfor; ?>
-		        	</div>
-				</div>			 
-
-        	</div>
-		</div>
-	</div>
-
-	<div class="homepage-middle-wrapper">
-		<!-- middle page wrapper for container -->
-		<div class="container homepage-middle">
-			
+	<div class="hidden-sm">
+		
+		<div class="container homepage-top"> 
+			<!--|____________| -->
 			<div class="row">
-			<!-- middle row -->			
-				<div class="col-xs-12">
-					<div class="h1 underline-title"><?=$news[$language][0]?></div>
-				</div>
 
-				<div class="col-xs-4">
-				<!-- Teaser Blocks -->	
-					<div class="row">
-						<div class="col-xs-12">
-							<?php for($count = 0; $count < 3; $count++): ?>
-			          			<div class="teaser-block">
-			          				<a href="construction-page.php">
-			          					<!-- Character space ~62 CHI -->
-			          					<!-- Character space ~104 ENG -->
-			          					<h4 class="<?=$test_teaser['colors'][$count]?>"><?=$test_teaser[$language][$count]?></h4>
+				<!-- Desktop display image -->
+				<div class="col-xs-12 text-center">
+					<?php if($language == "en"): ?> 
+						<img class="img-responsive full-width" src="/images/homepage_img_folder/mainPagePicENG.jpg" alt="" />
+					<?php elseif($language == "zh-hans") : ?>
+					    <img class="img-responsive full-width" src="/images/homepage_img_folder/mainPagePicCHI.jpg" alt="" />
+					<?php endif; ?>
+				</div>
+				<div class="col-xs-12 text-center homepage-links">
+				<!-- Block links -->
+					<div class="row homepage-links">
+						<div class="col-xs-10 col-xs-offset-1">
+							<!-- Use PHP to loop through an array to create link block images -->
+							<?php for($count = 0; $count < count($box_images); $count++): ?>
+			          			<div class="col-xs-3 block-link">
+			          				<a href="<?=$homepage_block_links[$count]?>" class="">
+			          					<img class="img-responsive" src="/images/homepage_img_folder/<?=$box_images[$count] ?>" alt="">
+			          					<h3 class="box-text"><?=$boxes_links[$language][$count]?></h3>
 			          				</a>
 			          			</div>
 			        		<?php endfor; ?>
-						</div>
-					</div>
-				</div>
+			        	</div>
+					</div>			 
 
-				<div class="col-xs-8">
-					<div class="row homepage-image-row">
-						<div class="col-xs-12">
-							<img src="/images/homepage_img_folder/mainPagePicSmall1.png" alt=""> <img src="/images/homepage_img_folder/mainPagePicSmall2.png" alt="">
-						</div>
-					</div>
-					<div class="row homepage-image-row">
-						<div class="col-xs-12">
-							<img src="/images/homepage_img_folder/mainPagePicSmall3.png" alt=""> <img src="/images/homepage_img_folder/mainPagePicSmall4.png" alt="">
-						</div>
-					</div>
-				</div>
+	        	</div>
+			</div>
+		</div>
 
-			</div>		
+		<div class="homepage-middle-wrapper">
+			<!-- middle page wrapper for container -->
+			<div class="container homepage-middle">
+				
+				<div class="row">
+				<!-- middle row -->			
+					<div class="col-xs-12">
+						<div class="h1 underline-title"><?=$news[$language][0]?></div>
+					</div>
+
+					<div class="col-xs-4">
+					<!-- Teaser Blocks -->	
+						<div class="row">
+							<div class="col-xs-12">
+								<?php for($count = 0; $count < 3; $count++): ?>
+				          			<div class="teaser-block">
+				          				<a href="construction-page.php">
+				          					<!-- Character space ~62 CHI -->
+				          					<!-- Character space ~104 ENG -->
+				          					<h4 class="<?=$test_teaser['colors'][$count]?>"><?=$test_teaser[$language][$count]?></h4>
+				          				</a>
+				          			</div>
+				        		<?php endfor; ?>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xs-8">
+						<div class="row homepage-image-row">
+							<div class="col-xs-12">
+								<img src="/images/homepage_img_folder/mainPagePicSmall1.png" alt=""> <img src="/images/homepage_img_folder/mainPagePicSmall2.png" alt="">
+							</div>
+						</div>
+						<div class="row homepage-image-row">
+							<div class="col-xs-12">
+								<img src="/images/homepage_img_folder/mainPagePicSmall3.png" alt=""> <img src="/images/homepage_img_folder/mainPagePicSmall4.png" alt="">
+							</div>
+						</div>
+					</div>
+
+				</div>		
+			</div>
 		</div>
 	</div>
 
@@ -115,10 +117,10 @@
 
 	<!-- **************************************************************************************************** -->	
 	<!-- Mobile main Content of homepage WIP -->
-	<div class="hidden-xs">
+	<div class="hidden-sm hidden-md hidden-lg">
 		
 		<!-- Mobile main title text -->
-		<div class="container visible-xs hidden-sm hidden-md hidden-lg">
+		<div class="container visible-xs">
 			<!--|____________| -->
 			<div class="row">
 				<div class="col-xs-12 text-center mobile-homepage-title">
@@ -133,7 +135,7 @@
 		</div>
 
 		<!-- Mobile homepage image -->
-		<div class="container visible-xs hidden-sm hidden-md hidden-lg">
+		<div class="container visible-xs">
 			<!--|____________| -->
 			<div class="row">
 				<!-- Mobile display image -->
@@ -145,7 +147,7 @@
 
 		<!-- Mobile info pages -->
 		<?php for($count = 0; $count < 4; $count++): ?>
-			<div class="mobile-home-info-page-wrapper visible-xs hidden-sm hidden-md hidden-lg">
+			<div class="mobile-home-info-page-wrapper visible-xs">
 				<div class="container">
 					<!--|____________| -->
 					<div class="row">
@@ -165,8 +167,8 @@
 		<!-- Mobile New page -->
 		<!-- Character space ~62 CHI -->
 		<!-- Adding text to mobile needs a new design style -->
-	<!-- 
-		<div class="mobile-home-info-page-wrapper visible-xs hidden-sm hidden-md hidden-lg">
+<!-- 	 
+		<div class="mobile-home-info-page-wrapper visible-xs">
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12 text-center mobile-news-page">
@@ -183,17 +185,16 @@
 				</div>
 			</div>
 		</div>
-	-->
-
+-->
 		<!-- Mobile bottom images page -->
-		<div class="container mobile-home-image-page-wrapper visible-xs hidden-sm hidden-md hidden-lg">
+		<div class="container mobile-home-image-page-wrapper visible-xs">
 			<!--|____________| -->
 			<div class="row">
 				<div class="col-xs-12 mobile-image"><img class="img-responsive full-width" src="images/mHomepage_Img_Folder/mImage_one.jpg" alt="" /></div>
 				<div class="col-xs-12 mobile-image"><img class="img-responsive full-width" src="images/mHomepage_Img_Folder/mImage_two.jpg" alt="" /></div>
 			</div>
 		</div>
-		<div class="container mobile-home-image-page-wrapper visible-xs hidden-sm hidden-md hidden-lg">
+		<div class="container mobile-home-image-page-wrapper visible-xs">
 			<!--|____________| -->
 			<div class="row">
 				<div class="col-xs-12 mobile-image"><img class="img-responsive full-width" src="images/mHomepage_Img_Folder/mImage_three.jpg" alt="" /></div>
